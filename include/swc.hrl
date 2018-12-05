a88156773c543227c3305016e8cd520c56e2010e
Andre Graf <andre@dergraf.org>
2018-12-05 13:19:38 +0100

    fix type specs

--- include/swc.hrl
+++ include/swc.hrl
@@ -1,15 +1,15 @@
 
--type vv()      :: #{Key :: id() => Entry :: counter()}.
+-type vv()      :: #{Key :: id() := Entry :: counter()}.
 
 -type dcc()     :: {dots(), vv()}.
 
--type bvv()     :: #{Key :: id() => Entry :: entry()}.
+-type bvv()     :: #{Key :: id() := Entry :: entry()}.
 
 -type key_matrix()  :: [{Node :: id(), DotKey :: [{counter(), id()}]}]. % orddict
 
--type vv_matrix() :: {map(), map()}.
+-type vv_matrix() :: {#{Key :: id() := Entry :: vv()} | #{}, #{Key :: id() := Entry :: vv()} | #{}}.
 
--type dots()    :: #{Dot :: dot() => Value:: value()}.
+-type dots()    :: #{Dot :: dot() := Value:: value()} | #{}.
 -type dot()     :: {id(), counter()}.
 -type entry()   :: {counter(), counter()}.
 -type id()      :: term().
